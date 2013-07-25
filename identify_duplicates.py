b5891b0a576c2f16e35e68a6fc739d1e9387b619
jfear <jfear@ufl.edu>
2013-07-25 15:19:19 -0400

    Re-worked the scirpt, still working on pcr dup count

--- identify_duplicates.py
+++ identify_duplicates.py
@@ -12,8 +12,8 @@ def getOptions():
     """Function to pull in command line arguments"""
     parser = argparse.ArgumentParser(description='Tool for identifying duplicates and creating various useful output')
     parser.add_argument('-i','--input',dest='fq', action='store', required=True, help='A list of fq file [Required]')
-    parser.add_argument('-t','--table',dest='table',action='store',required=False, help='Output table of intermediate size information')
     parser.add_argument('-o','--out', dest='out', action='store', required=True, help='Output file for counts in csv format [Required]')
+    parser.add_argument('--pixel', dest='pix', action='store', default=100, required=False, help='Number of pixels to consider a read as an optical duplicate [Default:100]')
     parser.add_argument('-g','--log',dest='log',action='store',required=False, help='Create an error log')
     args = parser.parse_args()
     return(args)
@@ -21,30 +21,77 @@ def getOptions():
 
 def setLogger(fname,loglevel):
     """Function for handling error logging"""
-    logging.basicConfig(filename=fname, level=loglevel, format='%asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename=fname, level=loglevel, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def readFASTQ(fname):
-    """Read a fastq file and store information in a dictionary"""
-    myDict = collections.defaultdict(lambda: collections.defaultdict(list))
+def readFASTQ(fname,count):
+    """Read a fastq file and store information in a dictionary where the key is
+       the sequence and the value is a list of headers. Also returns a total
+       number of reads."""
+
+    logging.info("Reading the FASTQ file.")
+    myDict = collections.defaultdict(list)
     with open(fname,'r') as FQ:
         for record in SeqIO.parse(FQ, 'fastq'):
-            myDict[str(record.seq)]['name'].append(record.name)
-            myList = parseHeader(record.name)
-            if len(myList) == 5:
-                lane, tile, xcord, ycord, pe = myList
-            elif len(myList) == 4:
-                lane, tile, xcord, ycord = myList
-                pe = 0
-            else:
-                logging.ERROR("malformed FASTQ header %s" % (record.name))
+            myDict[str(record.seq)].append(record.name)
+            count += 1
+    logging.info("Finished reading the FASTQ file.")
+    return(myDict,count)
 
-            if pe != 2:
-                myDict[str(record.seq)]['lane'].append(lane)
-                myDict[str(record.seq)]['tile'].append(tile)
-                coords = map(int,(xcord,ycord))
-                myDict[str(record.seq)]['coord'].append(numpy.array(coords))
-    return(myDict)
+
+def checkOptical(myList, pix):
+    """Given a list of FASTQ headers, this function will check if reads are
+       within a certain 'args.pix' distance of each other. If there are within
+       this distance, then they will be called optical duplicates."""
+
+    logging.info("Starting check for optical dups.")
+
+    # Create a dictionary to store optical duplicate information.
+    myDict = dict()
+    cntPcrDups = 0
+
+    # Create a list of sets, where each set contains headers that are within
+    # args.pix of each other.
+    listOfSets, cntPcrDups = identifySets(myList, pix, cntPcrDups)
+
+    # reduce the set list so that each set is a group of optical duplicates.
+    redSetList = reduceSet(listOfSets)
+
+    logging.info("Finished check for optical dups.")
+    return(redSetList,cntPcrDups)
+
+
+def identifySets(myList, pix, count):
+    """This function steps through a list of headers and creates sets of those
+       that fall within the args.pix range. These need to be reduced."""
+
+    logging.info("Creating sets of optical dups.")
+    # Create list to store results
+    setList = list()
+    # Compare each header and create sets of headers that overlap.
+    for item1 in myList:
+        # Parse the first header to grab coordinate information.
+        match1 = parseHeader(item1)
+        item1Set = {item1}
+        for item2 in myList:
+            # Don't compare a header to itself.
+            if item1 != item2: 
+                # Parse the second header to grab coordinate information.
+                match2 = parseHeader(item2)
+
+                # Test if headers are on the same lane and tile. If they are
+                # then determine if they are within args.pix of each other
+                # using numpy's norm function.
+                if match1[0] == match2[0] and match1[1] == match2[1]:
+                    myCoord = [numpy.array([int(match1[2]),int(match1[3])]),numpy.array([int(match2[2]),int(match2[3])])]       # create a list of numpy arrays [(x1,y1),(x2,y2)]
+                    eucDist = numpy.linalg.norm(myCoord[0]-myCoord[1])
+                    if eucDist <= pix:
+                        item1Set.add(item2)
+                    else:
+                        count += 1
+        setList.append(item1Set)
+    logging.info("Finished creating sets of optical dups.")
+    return(setList, count)
 
 
 def parseHeader(fqName):
@@ -54,25 +101,65 @@ def parseHeader(fqName):
     return(matchList)
 
 
-def calcDist(myCoord)
-    """Function to calculate the distance between all pairwise combinations of coordinates"""
-    myList = list()
-    myIter = itertools.combinations(myCoord,2)     # Create an iterator of all possible pairwise combinations
-    for record in myIter:
-        myList.append(numpy.linalg.norm(record[0]-record[1]))    # Calculate the distance between each set of coordinates using numpy's norm function
-    return(myList)
+def reduceSet(setList):
+    """Function to step through a list of sets and combine sets that overlap.
+       This will create a unique list of sets, where each set contains the headers
+       of reads that are within args.pix of each other"""
+
+    logging.info("Starting to reduce optical dup sets.")
+    setList2 = [setList[0]]
+    for item1 in setList[1:]:
+        inSetList2 = False
+        for item2 in setList2:
+            if item1 & item2:
+                item2 |= item1
+                inSetList2 = True
+                break
+        if not inSetList2:
+            setList2.append(item1)
+
+    logging.info("Finished reducing optical dup sets.")
+    return(setList2)
     
 
 def main():
     args = getOptions()
     if args.log:
         setLogger(args.log,logging.INFO)
-        setLogger(args.log,logging.ERROR)
-        setLogger(args.log,logging.WARNING)
 
-    myFASTQ = readFASTQ(args.fq)
+    total_read = 0     # Initialize a counter to keep track of the total number of reads
+    uniq_read = 0      # Initialize a counter to keep track of the total number of unique reads
+    pcrDup = 0
 
+    # Read in the FASTQ file and return a dictionary with SEQ as the key and
+    # list(HEADERS) as the values. Also return the total number of reads.
+    myFASTQ, total_read = readFASTQ(args.fq,total_read)
 
+    # Calculate the number of unique SEQUENCES. 
+    #NOTE: this is not the same as number of unique READS.
+    uniq_seq = len(myFASTQ)
+
+    # Loop through each sequence in the dictionary and examine it for
+    # duplicates and optical duplicates.
+    for key in myFASTQ:
+        # Copy the dictionary value using list() so that I don't modify the
+        # dictionary value later in the script.
+        myValue = list(myFASTQ[str(key)])   
+        myLength = len(myValue)
+
+        if myLength == 1:
+            # If there is only one header for a given sequence, then this is a unique read.
+            uniq_read += 1
+        elif myLength > 1:
+            # If there are more than one header for a given, then these are reads.
+            total_dup = myLength
+            # Identify if the duplicates are optical dups or something else.
+            redSet, cntPcrDup = checkOptical(myValue, args.pix)
+            pcrDup += cntPcrDup
+        else:
+            logging.ERROR("There is something wrong with the fastq dictionary at key %s" % key)
+
+    print redSet, total_read, pcrDup, total_dup, uniq_read
 
 if __name__ == '__main__':
     main()
@@ -103,5 +190,4 @@ if args.table:
    	    for item in sorted_counts:
                 tableOut.write(str(item[1])+' '+str(item[0]).strip()+'\n')
  
-
 """
