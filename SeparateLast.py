bec0860509f7e00b5e9160b2365d52ce64f340f8
jfear <jfear@ufl.edu>
2013-07-29 18:53:22 -0400

    The version of the script had a lot of bugs, I re-wrote with biopython to make things simpler

--- SeparateLast.py
+++ SeparateLast.py
@@ -1,23 +1,27 @@
 import os;
 import sys;
-if len(sys.argv) != 4:
-    print "Error, wrong number of arguments. \nUsage: SeparateLast input_file num_files num_lines"
+from Bio import SeqIO
+
+
+if len(sys.argv) != 3:
+    print "Error, wrong number of arguments. \nUsage: SeparateLast fastq_file num_files"
 else:
     input_file=sys.argv[1];
     num_files=int(sys.argv[2]); 
-    num_lines=int(sys.argv[3]);
 
     fileArray=[];
     for numFile in range(0, num_files):
         fileArray.append(open(str(numFile)+"-split.fq", 'w+'))
-    readline=0
-    writefile=0
-    #read each line of the input file and write it to one of the output files. Write each four lines to the next output file, round robin style, until done. 
+
+    writeFlag = 0
+
     with open(input_file) as read_input_file:
-        for writeline in read_input_file.readlines():
-            fileArray[writefile].write(writeline)
-            readline=readline+1
-            if readline%num_lines==0:
-                writefile=(writefile+1)%num_lines
+        for record in SeqIO.parse(read_input_file, 'fastq'):
+            SeqIO.write(record, fileArray[writeFlag], 'fastq')
+            if writeFlag < num_files-1:
+                writeFlag += 1
+            else:
+                writeFlag = 0
+
     for outfile in fileArray:
         outfile.close()
