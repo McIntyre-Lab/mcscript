b3e43b3bc6879409a1d97a763ab1893e5afcf3dc
Justin Fear <jfear@ufl.edu>
2013-07-29 18:20:53 -0400

    There are some bugs in this version of the SeparateLast script, trying to fix

--- SeparateLast.py
+++ SeparateLast.py
@@ -1,24 +1,23 @@
 import os;
 import sys;
 if len(sys.argv) != 4:
-	print "Error, wrong number of arguments. \nUsage: SeparateLast input_file num_files num_lines"
+    print "Error, wrong number of arguments. \nUsage: SeparateLast input_file num_files num_lines"
 else:
-	input_file=sys.argv[1];
-	num_files=int(sys.argv[2]); 
-	num_lines=int(sys.argv[3]);	
-	
-	fileArray=[];
-	for numFile in range(0, num_files):
-		fileArray.append(open(n+"-split.fq", 'w+'))
-	readline=0
-	writefile=0
- #read each line of the input file and write it to one of the output files. Write each four lines to the next output file, round robin style, until done. 
-	with open(input_file) as read_input_file:
-		for writeline in read_input_file.readlines():
-			fileArray[writefile].write(writeline)
-			readline=readline+1
-			if readline%num_lines==0:
-				writefile=(writefile+1)%num_lines
-	for outfile in fileArray:
-		outfile.close()
-	
+    input_file=sys.argv[1];
+    num_files=int(sys.argv[2]); 
+    num_lines=int(sys.argv[3]);
+
+    fileArray=[];
+    for numFile in range(0, num_files):
+        fileArray.append(open(str(numFile)+"-split.fq", 'w+'))
+    readline=0
+    writefile=0
+    #read each line of the input file and write it to one of the output files. Write each four lines to the next output file, round robin style, until done. 
+    with open(input_file) as read_input_file:
+        for writeline in read_input_file.readlines():
+            fileArray[writefile].write(writeline)
+            readline=readline+1
+            if readline%num_lines==0:
+                writefile=(writefile+1)%num_lines
+    for outfile in fileArray:
+        outfile.close()
