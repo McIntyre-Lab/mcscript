c12cbf2d84bfb4873e66edfaa4f0d70b548d1375
jfear <jfear@ufl.edu>
2013-09-23 14:45:49 -0400

    modified the fastqSplitDups script to add a counter to produce the various tables that alison's script made.

--- fastqSplitDups.py
+++ fastqSplitDups.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import argparse 
 import collections
+import itertools
+import operator
 import re
 import os.path
 import logging
@@ -16,6 +18,7 @@ def getOptions():
     parser.add_argument("-t", "--table", dest="tname", action='store', required=True, help="Output table of intermediate size information [Required]")
     parser.add_argument("-g", "--log", dest="log", action='store', required=False, help="Log File") 
     args = parser.parse_args()
+    #args = parser.parse_args(['-r1', '/home/jfear/tmp/fq/r1.fq', '-r2', '/home/jfear/tmp/fq/r2.fq', '--outdir', '/home/jfear/tmp/files', '-o', '/home/jfear/tmp/counts.csv', '-t', '/home/jfear/tmp/cnts_table.tsv'])
     return(args)
 
 def setLogger(fname,loglevel):
@@ -23,15 +26,24 @@ def setLogger(fname,loglevel):
     logging.basicConfig(filename=fname, level=loglevel, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def getName(odir, fname):
+    """ This function creates unique and duplicate dataset names. """
     bname = os.path.basename(fname)
     name = os.path.splitext(bname)[0]
     uname = os.path.join(odir, name + '_uniq.fq')
     dname = os.path.join(odir, name + '_duplicate.fq')
-    upuniq = os.path.join(odir, name + '_unpaired_uniq.fq')
-    updup = os.path.join(odir, name + '_unpaired_duplicate.fq')
-    return(uname, dname, upuniq, updup)
+    return(uname, dname)
+
+def getNameUnpaired(odir, fname1, fname2):
+    """ This function creates the unpaired dataset names. """
+    name1 = os.path.splitext(os.path.basename(fname1))[0]
+    name2 = os.path.splitext(os.path.basename(fname2))[0]
+    upuniq = os.path.join(odir, name1 + '_' + name2 + '_unpaired_uniq.fq')
+    updup = os.path.join(odir, name1 + '_' + name2 + '_unpaired_duplicate.fq')
+    return(upuniq, updup)
 
 def readFq(fname,hdict):
+    """ Function to read a FQ file, I used the FastqGeneralIterator for its
+    speeds since I don't need a full SeqIO object. """
     with open(fname, 'r') as FQ:
         for header, seq, qual in FastqGeneralIterator(FQ):
             if header.count(' '):
@@ -71,14 +83,33 @@ def writeOutputSE(hdict,sdict,uname,dname,oname,tname):
         if len(value) == 1:
             myout = buildOutSE(hdict,value)
             UNIQ.write(myout)
+            total_num += 1
+            num_uniq_reads += 1
+            uniq_num += 1
         else:
             myout = buildOutSE(hdict,value)
             DUPS.write(myout)
             total_num += len(value)
             uniq_num += 1
 
+    UNIQ.close()
+    DUPS.close()
+
     percent_uniq_seq = float(uniq_num) / float(total_num) * 100
     percent_uniq_reads = float(num_uniq_reads) / float(total_num) * 100
+
+    with open(oname, 'w') as ON:
+        ON.write("file_name,total_reads,num_unique_seq,per_uniq_seq,num_unique_reads,per_uniq_reads\n")
+        myout = [os.path.basename(oname),total_num,uniq_num,percent_uniq_seq,num_uniq_reads,percent_uniq_reads]
+        ON.write(','.join([str(x) for x in myout])+"\n")
+
+    counts = dict()
+    for item in sdict:
+        counts[item] = len(sdict[item])
+    with open(tname, 'w') as OT:
+        sorted_counts = sorted(counts.iteritems(), key=operator.itemgetter(1), reverse=True)
+        for item in sorted_counts:
+            OT.write(str(item[1]) + ' ' + str(item[0]).strip() + "\n")
 
 def buildOutPE(hdict,vlist):
     myout1 = ''
@@ -122,14 +153,28 @@ def writeOutputPE(hdict,sdict,uname,dname,upuniq,updup,oname,tname):
             total_num += len(value)
             uniq_num += 1
 
-    
+    UNIQ1.close()
+    DUPS1.close()
+    UNIQ2.close()
+    DUPS2.close()
+    UPU.close()
+    UPD.close()
+
     percent_uniq_seq = float(uniq_num) / float(total_num) * 100
     percent_uniq_reads = float(num_uniq_reads) / float(total_num) * 100
     with open(oname, 'w') as ON:
         ON.write("file_name,total_reads,num_unique_seq,per_uniq_seq,num_unique_reads,per_uniq_reads\n")
         myout = [os.path.basename(oname),total_num,uniq_num,percent_uniq_seq,num_uniq_reads,percent_uniq_reads]
         ON.write(','.join([str(x) for x in myout])+"\n")
-    #TODO write output for counts table
+
+    counts = dict()
+    for item in sdict:
+        counts[item] = len(sdict[item])
+    with open(tname, 'w') as OT:
+        sorted_counts = sorted(counts.iteritems(), key=operator.itemgetter(1), reverse=True)
+        for item in sorted_counts:
+            OT.write(str(item[1]) + ' ' + str(item[0]).strip() + "\n")
+
 
 def main():
     """ MAIN Function to execute everything """
@@ -143,7 +188,7 @@ def main():
     odir = os.path.abspath(args.odir)
 
     # Construct output files for read 1 or SE reads
-    uname1, dname1, upuniq, updup = getName(odir, args.r1)
+    uname1, dname1 = getName(odir, args.r1)
     uname = [uname1]
     dname = [dname1]
 
@@ -155,7 +200,8 @@ def main():
 
     if args.r2:
         # Construct output files for read 2
-        uname2, dname2, upuniq, updup = getName(odir, args.r2)
+        uname2, dname2 = getName(odir, args.r2)
+        upuniq, updup = getNameUnpaired(odir, args.r1, args.r2)
         uname.append(uname2)
         dname.append(dname2)
 
