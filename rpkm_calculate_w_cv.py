0026a86630b44663a3b1960f3e69e51d46a1722d
jfear <jfear@ufl.edu>
2013-11-26 14:33:23 -0500

    Fixed several bugs and increase spead drastically

--- rpkm_calculate_w_cv.py
+++ rpkm_calculate_w_cv.py
@@ -31,10 +31,10 @@ def getOptions():
     parser.add_argument("-m", "--mpileup", dest="mname", action='store', required=True, help="mpileup file [Required]",metavar="MPILEUP_FILE")
     parser.add_argument("-s", "--sam", dest="sname", action='store', required=True, help="sam alignment file [Required]", metavar="SAM_FILE")
     parser.add_argument("-b", "--bed", dest="bname", action='store', required=True, help="bed file (3 or 4 columns) [Required]", metavar="BED_FILE")
+    parser.add_argument("-c", "--cv", dest="cv", action='store_true', required=False, help="A flag to indicate if you want output for the coefficient of variation [OPTIONAL]")
     parser.add_argument("-g", "--log", dest="log", action='store', required=False, help="Log File", metavar="LOG_FILE") 
     parser.add_argument("-o", "--out", dest="out", action='store', required=True, help="Out File", metavar="OUT_FILE")
-    #args = parser.parse_args()
-    args = parser.parse_args(['-m','/home/jfear/tmp/coverage/aln_to_genome.3L.mpileup','-s','/home/jfear/tmp/coverage/aln_to_genome.sam', '-b', '/home/jfear/tmp/coverage/fb551_4col.bed','-o','/home/jfear/tmp/coverage/test_genome.csv','-g','/home/jfear/tmp/coverage/test.log'])
+    args = parser.parse_args()
     return(args)
 
 def setLogger(fname,loglevel):
@@ -85,13 +85,18 @@ def read_bed(args):
             bdict[fusion]['end'] = end
             bdict[fusion]['region_length'] = length + 1  # convert back to 1 based scale
             bdict[fusion]['count'] = np.zeros(length)    # create a holding vector of 0's as long as the region, I will replace the 0's with counts from the mpileup
-    return(bdict)
+    cdict = collections.defaultdict(dict)
+    for fusion in bdict:
+        chrom = bdict[fusion]['chrom']
+        start = bdict[fusion]['start']
+        end = bdict[fusion]['end']
+        cdict[chrom].update(dict((x,fusion) for x in xrange(start,end+1))) # create a look up dictionary by chromosome. This will make parsing the mpileup faster.
+    return(bdict,cdict)
 
 # MPILEUP Functions
-def read_mpileup(args,bdict):
+def read_mpileup(args,bdict,cdict):
     """ Read mpileup and store depth and length into dictionary """
     logging.info("Reading the Mpileup File '%s'." % args.mname)
-    curr = dict()
     with open(args.mname, 'r') as MPILEUP:
         reader = csv.reader(MPILEUP, delimiter='\t',quoting=csv.QUOTE_NONE)
         for row in reader:
@@ -99,25 +104,11 @@ def read_mpileup(args,bdict):
             mpos = int(row[1]) - 1  # mpileups are 1-based
             mdepth = int(row[3])
             try:
-                # Check if we are still in the same exonic region, if not change curr
-                if curr['chrom'] == mchrom and curr['start'] <= mpos and curr['end'] > mpos:
-                    loc = mpos - curr['start']
-                    curr['count'][loc] = mdepth
-                else:
-                    for fusion in bdict:
-                        if bdict[fusion]['chrom'] == mchrom and bdict[fusion]['start'] <= mpos and bdict[fusion]['end'] > mpos:
-                            curr = bdict[fusion]
-                            loc = mpos - curr['start']
-                            curr['count'][loc] = mdepth
-                            break
+                fusion = cdict[mchrom][mpos]
+                loc = mpos - bdict[fusion]['start']
+                bdict[fusion]['count'][loc] = mdepth
             except:
-                # The first time around we won't have curr set, so set it
-                for fusion in bdict:
-                    if bdict[fusion]['chrom'] == mchrom and bdict[fusion]['start'] <= mpos and bdict[fusion]['end'] > mpos:
-                        curr = bdict[fusion]
-                        loc = mpos - curr['start']
-                        curr['count'][loc] = mdepth
-                        break
+                continue
 
 # Coverage Functions
 def calc_coverage(bdict,num_mapped_reads,read_length):
@@ -150,11 +141,19 @@ def writeOutput(args,bdict,num_mapped_reads,read_length):
         well with SAS downstream. So I opted for the brute force method. """
     logging.info("Writing Output")
 
-    header = ['fusion_id','mapped_reads','read_length','region_length','region_depth','reads_in_region','apn','rpkm','mean','std','cv']
-    with open(args.out, 'wb') as OUT:
-        OUT.write(','.join(header) + '\n')
-        for key in bdict:
-            OUT.write(','.join(str(x) for x in [key,num_mapped_reads,read_length,bdict[key]['region_length'],bdict[key]['depth'],bdict[key]['reads_in_region'],bdict[key]['apn'],bdict[key]['rpkm'],bdict[key]['mean'],bdict[key]['std'],bdict[key]['cv']]) + '\n')
+    if args.cv:
+        header = ['fusion_id','mapped_reads','read_length','region_length','region_depth','reads_in_region','apn','rpkm','mean','std','cv']
+        with open(args.out, 'wb') as OUT:
+            OUT.write(','.join(header) + '\n')
+            for key in bdict:
+                OUT.write(','.join(str(x) for x in [key,num_mapped_reads,read_length,bdict[key]['region_length'],bdict[key]['depth'],bdict[key]['reads_in_region'],bdict[key]['apn'],bdict[key]['rpkm'],bdict[key]['mean'],bdict[key]['std'],bdict[key]['cv']]) + '\n')
+    else:
+        header = ['fusion_id','mapped_reads','read_length','region_length','region_depth','reads_in_region','apn','rpkm']
+        with open(args.out, 'wb') as OUT:
+            OUT.write(','.join(header) + '\n')
+            for key in bdict:
+                OUT.write(','.join(str(x) for x in [key,num_mapped_reads,read_length,bdict[key]['region_length'],bdict[key]['depth'],bdict[key]['reads_in_region'],bdict[key]['apn'],bdict[key]['rpkm']]) + '\n')
+
 
 def main():
     """ MAIN Function to execute everything """
@@ -163,8 +162,8 @@ def main():
         setLogger(args.log,logging.INFO)
 
     num_mapped_reads, read_length = read_sam(args)       # Use SAM file to count the number of mapped reads and the max read length
-    bdict = read_bed(args)                               # Read through BED file and create dictionary to sort all information.
-    read_mpileup(args,bdict)                             # Read Mpileup file and populate the bdict with pileup information
+    bdict,cdict = read_bed(args)                               # Read through BED file and create dictionary to sort all information.
+    read_mpileup(args,bdict,cdict)                             # Read Mpileup file and populate the bdict with pileup information
     calc_coverage(bdict,num_mapped_reads,read_length)    # Use information stored in bdict to calculate coverage (APN,RPKM) and other measures for the genomic region
     writeOutput(args,bdict,num_mapped_reads,read_length) # Write output to CSV file
 
