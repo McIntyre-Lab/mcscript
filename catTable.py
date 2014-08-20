8f1be29488a7c1d48a5fb117b05e159a545dbb4c
jfear <jfear@ufl.edu>
2014-08-20 16:14:30 -0400

    Added logic to handle files not incremented by a simple number.

--- catTable.py
+++ catTable.py
@@ -9,7 +9,8 @@ def getOptions():
     """ Function to pull in arguments """
     parser = argparse.ArgumentParser(description="Takes a set of tabular files (e.g. CSV, TSV) and concatenates together.")
     parser.add_argument("-f", dest="fname", action='store', nargs='*', required=True, help="List of files to concatneate [Required]")
-    parser.add_argument("-o", dest="odir", action='store', required=False, help="Output directory, if not provided will create output in the original file's folder [Optional]")
+    parser.add_argument("--odir", dest="odir", action='store', required=False, help="Output directory, if not provided will create output in the original file's folder [Optional]")
+    parser.add_argument("--oname", dest="outname", action='store', required=False, help="Name of output file, if not provided I will try to guess [Optional]")
     parser.add_argument("--header", dest="header", action='store_true', help="Indicate if the file in question has a header [Optional]")
     parser.add_argument("-g", "--log", dest="log", action='store', required=False, help="Path and name of log file [Optional]") 
     args = parser.parse_args()
@@ -44,10 +45,22 @@ def removeOldOutput(args):
 
 def createOutput(args):
     """ Use input file information to create an output file """
-    bname = os.path.splitext(os.path.basename(args.fname[0]))
-    sumName = re.sub('_\d+$', '_summary', bname[0])
-    args.oname = os.path.join(args.odir, sumName + bname[1])
-    logging.info("File will be output to: {0}".format(args.oname))
+    if args.outname:
+        args.oname = os.path.join(args.odir, args.outname)
+    else:
+        bname = os.path.splitext(os.path.basename(args.fname[0]))
+        try:
+            # Files names incremented: foo_1.csv, foo_2.csv
+            sumName = re.sub('_\d+$', '_summary', bname[0])
+        except:
+            try:
+                # Files name are incremented by fusion id: foo_S1000_SI.csv, foo_F1002_SI.csv
+                sumName = re.sub('[SF]\d+_SI$', '_summary', bname[0])
+            except:
+                # Everything else just append summary
+                sumName = bname[0] + '_summary'
+        args.oname = os.path.join(args.odir, sumName + bname[1])
+        logging.info("File will be output to: {0}".format(args.oname))
 
     # remove old output if it is there
     removeOldOutput(args)
@@ -56,7 +69,6 @@ def getHeader(args):
     """ Pull the header from the first file """
     with open(args.fname[0], 'r') as INPUT:
         args.header = INPUT.readline()
-
 
 def main(args):
     # If an output directory was not given then put output in the same
@@ -69,7 +81,6 @@ def main(args):
 
     # get the list of files to process
     logging.info("A total of {0} files will be combined".format(len(args.fname)))
-
 
     with open(args.oname, 'w') as OUT:
         # If header was passed pull the header line
