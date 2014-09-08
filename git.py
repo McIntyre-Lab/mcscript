e12dac973fb747ed1a9232c89dc749f81b546373
jfear <jfear@ufl.edu>
2014-09-08 11:27:40 -0400

    Added functions for dealing with logs and git

---
+++ git.py
@@ -0,0 +1,20 @@
+import logging
+import subprocess
+
+def get_git():
+    """ Parse the current Git commit version. This will
+    allow recording of exactly which script was used to create a given
+    output.
+    """
+    # get full path to script
+    fullname = os.path.abspath(__file__)
+    gitdir = os.path.dirname(fullname)
+    label = subprocess.check_output(["git", "--git-dir="+gitdir+"/.git", "--work-tree="+gitdir,"rev-parse","HEAD"])
+    return(label.rstrip(), gitdir)
+
+def git_to_log():
+    """ Write current git commit information to the log. """
+    git_status, gitdir = getGit()
+    logging.info("Starting %s", __file__) 
+    logging.info("Running script from  %s", gitdir) 
+    logging.info("Git commit id: %s", git_status)
---
+++ logger.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+
+def set_logger(fname=None,logLevel=info):
+    """ Function to set up the handle error logging.
+    fname = the name of the log file, if no logfile is given then logging
+            information will be pritned to stdout.
+
+    logLevel = level of information to print out, options are {info, debug} [Default: info]
+    """
+
+    # Determine log level
+    if logLevel == 'info':
+        _log = logging.INFO
+    elif logLevel == 'debug':
+        _log = logging.DEBUG
+
+    # Output to file or STDOUT
+    if fname:
+        logging.basicConfig(filename=fname, filemode='w', level=_log, format='%(asctime)s - %(levelname)s - %(message)s')
+    else:
+        logging.basicConfig(stream=sys.stdout, level=_log, format='%(asctime)s - %(levelname)s - %(message)s')
