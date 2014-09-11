f8abedbc037dce4dc6bcaac75d97a2562da0cbad
jfear <jfear@ufl.edu>
2014-09-11 09:08:52 -0400

    Initial version of wigglePlot is ready

--- mclib/wiggle.py
+++ mclib/wiggle.py
@@ -87,98 +87,36 @@ class GeneModel(object):
             start += step
         return yList
 
+def plot_wiggle(pileDict, geneModel, outName):
+    """ Function to construct a wiggle plot with gene models
+    Arguments:
+    pileDict (dict) = where keys are genome coordinates and values are counts at that position
+    geneModel (obj) = a mclib/wiggle.py GeneModel object
+    outName (str) = name of the output png
+    """
 
+    # Set up the figure
+    fig = plt.figure(figsize=(20, 10))
 
-
-
-
-
-
-
-
-
-
-def drawExon(exonTupleList, exonDict, exoncolors, introns):
-	""" Draws an exon at the proper location. """
-	# Rectangle (xpos (left), ypos (bottom), width, height, kwargs)
-	patches = [Rectangle((0, -1), 1, 2, fill=False)] # initialize patches with a blank rectangle to avoid errors
-	loc = 0 
-	exonColor = 1
-
-	for exon in exonTupleList:
-		# width assumes exclusive nature for exons: that is, exon (20,22) is [20,22) and a width 1 rectangle will be drawn
-		if introns: # if drawing introns
-			start = exonDict[exon[0]] # make the start the location given by the exonDict
-			width = exonDict[exon[1]-1] - exonDict[exon[0]] # make the width equal to the width between the locations
-		else: # if drawing only exons
-			start = loc # make the start at our current location
-			width = exon[1] - exon[0] - 1 # make the width our distance between pairs
-		# for exon colors
-		if exonColor % 2 == 1: col = exoncolors[0] # if odd, use exoncolor1
-		else: col = exoncolors[1] # if even, use exoncolor2
-			
-		patches.append(Rectangle((start, -1), width, 2, color=col)) # draw the exon
-		loc += width + 1 # move the current location
-		exonColor += 1
-	return PatchCollection(patches, match_original=True)
-
-def SetupPlot(plotSize, dimensions, title, chrom, codons):
-	'''Set up the parameters for the graph.  Receives dimensions [xmin, xmax, ymin, ymax], alleleColor [color1, color2], title for the graph, and chromosome number.'''
-	fig = plt.figure(figsize=(plotSize[0], plotSize[1]))# set window size to width, height 
-	# add axes in rectangle left position, bottom position, width, height
-	ax1 = fig.add_axes([0.1, 0.3, 0.8, 0.6])
-	ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.2], sharex=ax1)
-	ax1.axis(dimensions) # set up axis ranges
-	ax2.set_ylim(-5, 5)
-	# set up titles, labels, and ticks
-	ax1.set_title(title)
-	if codons: ax2.set_xlabel("Chromosome %s (codons from start of gene)"%chrom)
-	else: ax2.set_xlabel("Chromosome %s (bp from start of gene)"%chrom)
-	ax1.grid(True)		
-	ax1.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: str(int((abs(x))))))# set ticks to absolute value
-	# if we choose to graph codons, then divide all ticks by 3
-	if codons: ax2.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: round(x/3, 1)))
-	ax2.set_yticks([]) # eliminate yticks from the 2nd axis
-	for label in ax1.xaxis.get_ticklabels():
-		label.set_fontsize(0)# eliminate the labels from our top plot without eliminating them from the bottom
-	horizontalLine = Line2D([-10000000000, 10000000000], [0, 0], linewidth=1, color='black')# draw horizontal x-axis
-	ax1.add_line(horizontalLine)
-
-	return ax1, ax2, fig
-
-if __name__ == '__main__':
-
-    from mclib import gff as mcgff
-    from mclib import wiggle as mcwiggle
-
-    fname = '/home/jfear/storage/useful_dmel_data/dmel-all-no-analysis-r5.51.gff'
-    myGff = mcgff.FlyGff(fname)
-    myGene = mcgff.FlyGene('InR', myGff)
-    model = mcwiggle.GeneModel(myGene)
-
-
-
-
-
-
-
-
-
-
-
-
-    fig = plt.figure(figsize=(10, 10))
+    # Set up the two subplots
+    ## ax1 will be the wiggle
     ax1 = fig.add_subplot(211)
-    ax1.set_ylim(0, 200)    # NOTE: set ylim equal to 0, max
-    ax1.set_xlim(model.xLoc[0]-100, model.xLoc[1]+100) # NOTE: set xlim equal to start-100, end-100
+    ax1.set_ylim(0, max(pileDict.values())+100)    # NOTE: set ylim equal to 0, max
+    ax1.set_xlim(geneModel.xLoc[0]-300, geneModel.xLoc[1]+300) # NOTE: set xlim equal to start-300, end-300
+
+    ## ax2 will be the gene model
     ax2 = fig.add_subplot(212, sharex=ax1)
-    ax2.set_ylim(min(model.yLoc), max(model.yLoc)+5)
+    ax2.set_ylim(min(geneModel.yLoc), max(geneModel.yLoc)+5)
     ax2.get_yaxis().set_visible(False)  # Hide y-axis on gene model plot
     ax2.get_xaxis().set_visible(False)  # Hide y-axis on gene model plot
-    p = PatchCollection(model.patches, match_original=True)
+
+    # Create the wiggle plot
+    n, bins, patches = ax1.hist(pileDict.keys(), bins=len(pileDict.keys()), weights=pileDict.values())
+
+    # Put the gene models together and create their plots
+    p = PatchCollection(geneModel.patches, match_original=True)
     ax2.add_collection(p)
-    #ax2.add_line(Line2D((10, 20), (2,2),linewidth=2,color='black'))
+
+    # Save output
     plt.show()
-    #fig.savefig('/home/jfear/Desktop/test.png')
-
-
+    fig.savefig(outName)
--- wigglePlot.py
+++ wigglePlot.py
@@ -19,63 +19,57 @@ from mclib import wiggle as mcwiggle
 
 def getOptions():
     """ Function to pull in arguments """
-    description = """"This script starts with a begining pathway in the format
-    of a 'path' file. It then builds SAS code for SEMs after adding a new gene
-    to all possible locations within a network.
-
-        (a) Downstream of endogenous genes
-        (b) Downstream of exogenous genes
-        (c) Upstream of endogenous genes
-        (d) Upstream of exogenous genes
-        (e) In between exogenous genes and their endogenous targets
-        (e) In betwee endogenous genes and their endogenous targets
+    description = """ This script constructs wiggle plots and gene models.
+    Wiggle plots are created from sorted BAM files. Gene models require a GFF
+    file at this time.
     """
 
     parser = argparse.ArgumentParser(description=description, formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument("-p", dest="pname", action='store', required=True, help="Name of the 'path' file [Required]")
-    parser.add_argument("-l", dest="lname", action='store', required=True, help="Path to sas library that has the SAS dataset that will be analyzed [Required]")
-    parser.add_argument("-m", dest="mname", action='store', required=True, help="Name of SAS dataset that will be analyzed [Required]")
-    parser.add_argument("-o", dest="oname", action='store', required=True, help="Name with PATH, of the output file ending with '.sas'; NOTE: model number will be appended to the filename [Required]")
-    parser.add_argument("-n", dest="newGene", nargs='+', action='store', required=True, help="List of new gene isoforms separted by spaces [Required]")
-    parser.add_argument("-g", dest="gname", action='store', required=True, help="The gene name of the new gene/isoforms being added [Required]")
-    parser.add_argument("-t", dest="template", action='store', required=False, help="Name of the PROC CALIS template file [Optional]")
+    parser.add_argument("--gff", dest="gffName", action='store', required=True, help="Name with PATH, to a GFF file. Note if the GFFutils database has not been created it will be, this could take around 30min [Required]")
+    parser.add_argument("--bam", nargs='*', dest="bamList", action='store', required=True, help="Name with PATH, to sorted BAM files that will be averaged. [Required]")
+    parser.add_argument("-g", dest="geneName", action='store', required=True, help="Name of the gene you want to make wiggles of. [Required]")
+    parser.add_argument("-o", dest="oname", action='store', required=True, help="Name of the output PNG. [Required]")
     parser.add_argument("--log", dest="log", action='store', required=False, help="Name of the log file [Optional]; NOTE: if no file is provided logging information will be output to STDOUT") 
 
-    args = parser.parse_args()
+    #args = parser.parse_args()
+    args = parser.parse_args(['--gff', '/home/jfear/storage/useful_dmel_data/dmel-all-no-analysis-r5.51.gff', 
+                              '--bam','/mnt/storage/cegs_aln/bam_fb551_genome_nodup/r101_V1.sorted.bam','/mnt/storage/cegs_aln/bam_fb551_genome_nodup/r101_V2.sorted.bam',
+                              '-g', 'InR',
+                              '-o', '/home/jfear/tmp/inr.png'])
     return(args)
 
 if __name__ == '__main__':
-    #args = getOptions()
+    args = getOptions()
 
     # Turn on logging
     mclib.logger.set_logger(args.log)
-    try:
-        # Output git commit version to log, if user has access
-        mclib.git.git_to_log()
-    except:
-        pass
+    # Output git commit version to log, if user has access
+    mclib.git.git_to_log()
 
-    # Grab gene annotation 
+    # GENE ANNOTATION 
+
     ## Import GFF database
-    gffName = '/home/jfear/storage/useful_dmel_data/dmel-all-no-analysis-r5.51.gff'
-    myGffDb = mcgff.FlyGff(gffName)
+    myGffDb = mcgff.FlyGff(args.gffName)
 
     ## Pull gene from database
-    myGene = mcgff.FlyGene('InR', myGffDb)
+    myGene = mcgff.FlyGene(args.geneName, myGffDb)
+
+    ## Create Gene Model
+    myModel = mcwiggle.GeneModel(myGene)
 
     ################################################################################
+    # GENE COVERAGE
 
-    # Pull in pileup 
-    bamName1 = '/mnt/storage/cegs_aln/bam_fb551_genome_nodup/r101_V1.sorted.bam'
-    bamName2 = '/mnt/storage/cegs_aln/bam_fb551_genome_nodup/r101_V2.sorted.bam'
+    # Pull in bam file and make gene pileup
+    bList = []
+    for bam in args.bamList:
+        bList.append(mcbam.Bam(bam, myGene.chrom, myGene.start, myGene.end, fudgeFactor=True))
 
-    b1 = mcbam.Bam(bamName1, myGene.chrom, myGene.start, myGene.end, fudgeFactor=True)
-    b2 = mcbam.Bam(bamName2, myGene.chrom, myGene.start, myGene.end, fudgeFactor=True)
-
-    b = mcbam.avg_pileups([b1,b2], fudgeFactor=True)
+    # Average Pileups together
+    avgPileup = mcbam.avg_pileups(bList, fudgeFactor=True)
     
+    ################################################################################
+    # MAKE WIGGLES
 
-print sum((Counter(dict(x.pileups)) for x in [b1, b2]), Counter())
-
-
+    mcwiggle.plot_wiggle(avgPileup, myModel, args.oname)
 
