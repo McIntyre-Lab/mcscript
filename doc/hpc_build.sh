f085783be3f113b629b3694ee3eb3846dc7a2370
Justin Fear <jfear@ufl.edu>
2014-09-14 17:38:37 -0400

    added some logic to the build script

--- doc/hpc_build.sh
+++ doc/hpc_build.sh
@@ -10,4 +10,10 @@ module load python/2.7.6
 SOURCE=/scratch/lfs/mcintyre/python.git/doc
 TARGET=/bio/mcintyre/mcpublic/mcpython
 
+# Remove old version
+rm -r $TARGET/*
+
 sphinx-build -E -b html $SOURCE $TARGET
+
+# Fix Permission
+chmod -R a+r $TARGET
