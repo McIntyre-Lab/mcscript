5c0c4ba13ee7a61dd367b0ec1cf99efddfbaa9db
jfear <jfear@ufl.edu>
2014-09-16 10:55:15 -0400

    Added option to use the fudgefactor or not

--- wigglePlot.py
+++ wigglePlot.py
@@ -35,6 +35,7 @@ def getOptions():
     parser.add_argument("--bam", nargs='*', dest="bamList", action='store', required=True, help="Name with PATH, to sorted BAM files that will be averaged. [Required]")
     parser.add_argument("-g", dest="geneName", action='store', required=True, help="Name of the gene you want to make wiggles of. [Required]")
     parser.add_argument("-o", dest="oname", action='store', required=True, help="Name of the output PNG. [Required]")
+    parser.add_argument("--fudge", nargs='*', dest="ff", action='store_true', required=False, help="Use the built in fudge factor. [Optional]")
     parser.add_argument("--log", dest="log", action='store', required=False, help="Name of the log file [Optional]; NOTE: if no file is provided logging information will be output to STDOUT") 
     args = parser.parse_args()
     #args = parser.parse_args(['--gff', '/home/jfear/storage/useful_dmel_data/dmel-all-no-analysis-r5.51.gff', '--bam','/mnt/storage/cegs_aln/bam_fb551_genome_nodup/r101_V1.sorted.bam','/mnt/storage/cegs_aln/bam_fb551_genome_nodup/r101_V2.sorted.bam', '-g', 'InR', '-o', '/home/jfear/tmp/inr.png'])
@@ -66,10 +67,10 @@ if __name__ == '__main__':
     # Pull in bam file and make gene pileup
     bList = []
     for bam in args.bamList:
-        bList.append(mcbam.Bam(bam, myGene.chrom, myGene.start, myGene.end, fudgeFactor=True))
+        bList.append(mcbam.Bam(bam, myGene.chrom, myGene.start, myGene.end, fudgeFactor=args.ff))
 
     # Average Pileups together
-    avgPileup = mcbam.avg_pileups(bList, fudgeFactor=True)
+    avgPileup = mcbam.avg_pileups(bList, fudgeFactor=args.ff)
     
     ################################################################################
     # MAKE WIGGLES
