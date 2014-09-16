cd8b8b79dcbcd10899551e38accc1c800975100d
jfear <jfear@ufl.edu>
2014-09-16 11:30:42 -0400

    Set up pretty git log.

--- git.py
+++ git.py
@@ -2,23 +2,25 @@ import logging
 import subprocess
 import os.path
 
-def get_git():
+def get_git(sname):
     """ Parse the current Git commit version. This will
     allow recording of exactly which script was used to create a given
     output.
     """
     # get full path to script
-    fullname = os.path.abspath(__file__)
+    fullname = os.path.abspath(sname)
     gitdir = os.path.dirname(fullname)
-    label = subprocess.check_output(["git", "log", "-n1", "--pretty=%h", fullname])
+    label = subprocess.check_output(["git", "--git-dir="+gitdir+"/.git", "--work-tree="+gitdir,"log", "-n1", "--pretty=%h", fullname])
+
     return(label.rstrip(), gitdir)
 
-def git_to_log():
+def git_to_log(sname):
     """ Write current git commit information to the log. """
     try:
-        git_status, gitdir = get_git()
-        logging.info("Starting %s", __file__) 
+        git_status, gitdir = get_git(sname)
+        logging.info("Starting %s", sname) 
         logging.info("Running script from  %s", gitdir) 
         logging.info("Git commit id: %s", git_status)
     except:
+        print "You need access to the repository to output git information."
         pass
