6921772d807e055af62506c37f0c5842a2a08327
Justin Fear <jfear@ufl.edu>
2014-11-15 13:06:48 -0500

    Updated logger to separate error output to stderr

--- logger.py
+++ logger.py
@@ -1,8 +1,10 @@
 import logging
 import sys
 
-def set_logger(fname=None,logLevel='info'):
+def setLogger(logger, fname=None, logLevel='info'):
     """ Function to set up the handle error logging.
+    logger (obj) = a logger object
+
     fname = the name of the log file, if no logfile is given then logging
             information will be pritned to stdout.
 
@@ -11,12 +13,36 @@ def set_logger(fname=None,logLevel='info'):
 
     # Determine log level
     if logLevel == 'info':
-        _log = logging.INFO
+        _level = logging.INFO
     elif logLevel == 'debug':
-        _log = logging.DEBUG
+        _level = logging.DEBUG
+
+    # Set the level in logger
+    logger.setLevel(_level)
+
+    # Set the log format
+    logfmt = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
     # Output to file or STDOUT
     if fname:
-        logging.basicConfig(filename=fname, filemode='w', level=_log, format='%(asctime)s - %(levelname)s - %(message)s')
+        fh = logging.FileHandler(filename=fname,mode='w')
+        fh.setLevel(_level)
+        fh.setFormatter(logfmt)
+        logger.addHandler(fh)
     else:
-        logging.basicConfig(stream=sys.stderr, level=_log, format='%(asctime)s - %(levelname)s - %(message)s')
+        # Since no file was provide output to STDOUT and STDERR
+        loghandler = logging.StreamHandler(stream=sys.stdout)
+        errhandler = logging.StreamHandler(stream=sys.stderr)
+
+        # Set logging level for the different output handlers. 
+        # ERRORs to STDERR, and everything else to STDOUT
+        loghandler.setLevel(_level)
+        errhandler.setLevel(logging.ERROR)
+
+        # Format the log handlers
+        loghandler.setFormatter(logfmt)
+        errhandler.setFormatter(logfmt)
+
+        # Add handler to the main logger
+        logger.addHandler(loghandler)
+        logger.addHandler(errhandler)
