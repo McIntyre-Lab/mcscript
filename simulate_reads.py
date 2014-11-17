f32e61e1aba4b0bfa76b196e97dcae2c0b16ef12
Justin Fear <jfear@ufl.edu>
2014-11-17 14:25:14 -0500

    Added prefix option

--- simulate_reads.py
+++ simulate_reads.py
@@ -19,6 +19,7 @@ def getOptions():
     parser.add_argument('-i','--input',dest='fname', action='store', required=True, help='FASTA file [Required]')
     parser.add_argument('-o','--out', dest='oname', action='store', required=True, help='FASTQ file with simulated reads from FASTA [Required]')
     parser.add_argument('-n','--num', dest='n', action='store', required=True, help='Read length for simulation [Required]')
+    parser.add_argument('--prefix', dest='prefix', action='store', required=False, help='A prefix to add to the read id for distinguishing where reads came from [Optional]')
     parser.add_argument('-g','--log',dest='log',action='store',required=False, help='Create an error log')
     parser.add_argument("--debug", dest="debug", action='store_true', required=False, help="Enable debug output.") 
     args = parser.parse_args()
@@ -47,7 +48,10 @@ def main(args):
                 for winSeq in winGen:
                     currseq = ''.join(winSeq)
                     if len(currseq) >= 25:
-                        currid = "%s:%s-%s" % (record.id,start,end)
+                        if args.prefix:
+                            currid = "%s_%s:%s-%s" % (args.prefix,record.id,start,end)
+                        else:
+                            currid = "%s:%s-%s" % (record.id,start,end)
                         currqual = ''.join(['g']*len(currseq))
                         curr = "@%s\n%s\n+\n%s\n" % (currid, currseq, currqual)
                         OUT.write(curr)
