d20cf481a7dfcdf3753533faffc8a29b50e45e72
Justin Fear <jfear@ufl.edu>
2015-03-09 16:52:52 -0400

    Moved semnet to new repository

--- semnet/__init__.py
+++
@@ -1,5 +0,0 @@
-from semnet.parser import createPath
-from semnet.interfaces import SemPath, NewGene
-from semnet import models_baseline
-from semnet import models_add
-from semnet import models_newlink
