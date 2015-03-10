9dd9ec12d1509aa630b7e2f941982c7efd012851
Justin Fear <jfear@ufl.edu>
2015-03-10 15:07:15 -0400

    Modified hpc build script.

--- doc/hpc_build.sh
+++ doc/hpc_build.sh
@@ -6,14 +6,40 @@
 # 
 #===============================================================================
 
-module load python/2.7.6
-SOURCE=/scratch/lfs/mcintyre/python.git/doc
-TARGET=/bio/mcintyre/mcpublic/mcpython
+# Only run this on the hpc
+if [[ $HOSTNAME == *ufhpc* ]]; then
 
-# Remove old version
-rm -r $TARGET
+    module load python/2.7.6
 
-sphinx-build -E -b html $SOURCE $TARGET
+    SOURCE=/scratch/lfs/mcintyre/python.git/doc
+    TARGET=/bio/mcintyre/mcpublic/mcpython/mcscript
 
-# Fix Permission
-chmod -R 775 $TARGET
+
+    # Move to doc folder
+    cd $SOURCE
+
+    # Prepare target folder
+    if [ ! -e $TARGET ]; then
+        mkdir -p $TARGET
+    else
+        rm -rf $TARGET/*
+    fi
+
+    # Make sure build folder is clean
+    make clean
+
+    # Build html docs
+    make html
+
+    # Move to public
+    rsync -av $SOURCE/_build/html $TARGET
+
+    find $TARGET -type d -exec chmod 775 {} \;
+    find $TARGET -type f -exec chmod 664 {} \;
+
+    # Clean up build folder
+    make clean
+
+else
+    echo "To run this script you need to be on the hpc."
+fi
